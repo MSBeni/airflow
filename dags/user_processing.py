097eba495752e77f162164ec85fe22c37cb1b4b3
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-15 20:35:45 -0500

    Adding th whole project

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,4 +1,5 @@
 from airflow.models import DAG
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from datetime import datetime
 
 default_args = {
@@ -7,4 +8,17 @@ default_args = {
 
 with DAG('user_processing', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
     # define tasks/operators
-    pass
+    creating_table = SqliteOperator(
+        task_id='creating_table',        # Ought to be unique -- each task one unique id
+        sqlite_conn_id='db_sqlite',
+        sql='''
+            CREATE TABLE users (
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                country TEXT NOT NULL,
+                user_name TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL PRIMARY KEY     
+            );
+        '''
+    )
