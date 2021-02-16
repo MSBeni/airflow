d8ca4070d6d1e33233209d5bfc92f906d055c2a6
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-15 19:50:43 -0500

    Added the dags file

---
+++ dags/user_processing.py
@@ -0,0 +1,10 @@
+from airflow.models import DAG
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020, 1, 1)
+}
+
+with DAG('user_processing', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
+    # define tasks/operators
+    pass
