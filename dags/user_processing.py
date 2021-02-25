b422b92d1854d81f3156ddc8b2605069c07a8a93
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-25 17:13:56 -0500

    Added python operatior

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -2,12 +2,19 @@ from airflow.models import DAG
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
 from datetime import datetime
 import json
 
 default_args = {
     'start_date': datetime(2020, 1, 1)
 }
+
+
+def _processing_user():
+    pass
+
 
 # each DAG out to has a unique dag_id
 with DAG(dag_id='user_processing', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
@@ -33,6 +40,7 @@ with DAG(dag_id='user_processing', schedule_interval='@daily', default_args=defa
         endpoint='api/'
     )
 
+    # Extract the user from api
     extracting_user = SimpleHttpOperator(
         task_id='user_extraction',
         http_conn_id='user_api',
@@ -41,3 +49,13 @@ with DAG(dag_id='user_processing', schedule_interval='@daily', default_args=defa
         response_filter=lambda response: json.loads(response.text),
         log_response=True
     )
+
+    processing_user = PythonOperator(
+        task_id='processing_user',
+        python_callable=_processing_user
+    )
+
+
+
+
+    
