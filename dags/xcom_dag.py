c45629ac9b340cac5de19510af675edaee0cd00e
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-03-18 20:23:49 -0400

    Added XComs DAG 3

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -7,8 +7,15 @@ from airflow.utils.task_group import TaskGroup
 from random import uniform
 from datetime import datetime
 
-def _tarining_model():
-    pass
+
+def _training_model():
+    accuracy = uniform(0.1, 10.0)
+    return f'model\'s accuracy is {accuracy}'
+
+
+def _choose_best_model():
+    print("Choose Best Model")
+
 
 default_args = {
     'start_sate': datetime(2020, 1, 1)
