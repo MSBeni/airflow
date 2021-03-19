153b31ddf58e06cd38848afdd42a1a1712f3a9f7
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-03-18 21:01:33 -0400

    Added XComs DAG 4

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.operators.subdag import SubDagOperator
 from airflow.utils.task_group import TaskGroup
 
 from random import uniform
@@ -21,4 +20,29 @@ default_args = {
     'start_sate': datetime(2020, 1, 1)
 }
 with DAG(dag_id='xcom_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
-    pass
+    downloading_date = BashOperator(
+        task_id='downloading_date',
+        bash_command='sleep 3'
+    )
+
+    with TaskGroup('processing_tasks') as processing_tasks:
+        training_model_1 = PythonOperator(
+            task_id='training_model_1',
+            python_callable=_training_model
+        )
+        training_model_2 = PythonOperator(
+            task_id='training_model_2',
+            python_callable=_training_model
+        )
+        training_model_3 = PythonOperator(
+            task_id='training_model_3',
+            python_callable=_training_model
+        )
+
+    chosen_model = PythonOperator(
+        task_id='chosen_model',
+        bash_command=_choose_best_model
+    )
+
+    downloading_date >> processing_tasks >> chosen_model
+
