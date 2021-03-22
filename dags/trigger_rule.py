d64d9523b832b108d3cf3fe4393435f60581b90f
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-03-22 17:08:03 -0400

    Added trigger rule dag 5

--- dags/trigger_rule.py
+++ dags/trigger_rule.py
@@ -9,20 +9,21 @@ default_args = {
 with DAG(dag_id='trigger_rule', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
     task_1 = BashOperator(
         task_id='task_1',
-        bash_command='exit 0',
+        bash_command='exit 1',
         do_xcom_push=False
     )
 
     task_2 = BashOperator(
         task_id='task_2',
-        bash_command='exit 0',
+        bash_command='exit 1',
         do_xcom_push=False
     )
 
     task_3 = BashOperator(
         task_id='task_3',
         bash_command='exit 0',
-        do_xcom_push=False
+        do_xcom_push=False,
+        trigger_rule='all_failed'
     )
 
     [task_1, task_2] >> task_3
