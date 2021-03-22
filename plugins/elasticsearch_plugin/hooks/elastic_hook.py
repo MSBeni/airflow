4f01f43486c1c94906e5451445f2505d25a6cb59
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-03-22 17:53:26 -0400

    Added elastic hook

--- plugins/elasticsearch_plugin/hooks/elastic_hook.py
+++ plugins/elasticsearch_plugin/hooks/elastic_hook.py
@@ -1,2 +1,21 @@
 from airflow.hooks.base import BaseHook
 from elasticsearch import Elasticsearch
+
+class ElasticHook(BaseHook):
+    def __init__(self, conn_id='elasticsearch_default', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        conn = self.get_connection(conn_id)
+
+        conn_config = {}
+        hosts = []
+
+        if conn.host:
+            hosts = conn.host.split(',')
+
+        if conn.port:
+            conn_config['port'] = int(conn.port)
+
+        if conn.login:
+            conn_config['http_auth'] = (conn.login, conn.password)
+
+
