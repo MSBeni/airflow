d60d201f82edfb1ad888ec8d424cbf19259390d9
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-03-22 17:04:36 -0400

    Added trigger rule dag 3

--- dags/trigger_rule.py
+++ dags/trigger_rule.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 default_args = {
@@ -7,4 +7,22 @@ default_args = {
 }
 
 with DAG(dag_id='trigger_rule', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
-    pass
+    task_1 = BashOperator(
+        task_id='task_1',
+        bash_command='exit 0',
+        do_xcom_push=False
+    )
+
+    task_2 = BashOperator(
+        task_id='task_2',
+        bash_command='exit 0',
+        do_xcom_push=False
+    )
+
+    task_3 = BashOperator(
+        task_id='task_3',
+        bash_command='exit 0',
+        do_xcom_push=False
+    )
+
+    [task_1, task_2] >> task_3
