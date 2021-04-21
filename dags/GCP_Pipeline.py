dd7ac75939084e89562a8ad4be5028ab286eca9c
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-04-21 15:46:10 -0400

    Added new pipeline for GCP

--- dags/GCP_Pipeline.py
+++ dags/GCP_Pipeline.py
@@ -3,5 +3,8 @@ from datetime import datetime, timedelta
 from airflow.contrib.operators.dataflow_operator import DataFlowPythonOperator
 from airflow.operators.python import PythonOperator
 
-default_args = {}
+default_args = {
+    'owner': 'Airflow',
+
+}
 
